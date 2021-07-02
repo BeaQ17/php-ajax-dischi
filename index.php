62b92e7fffe92f4f947a275f68a0b402135d0762
BeaQ17 <beaq235@gmail.com>
2021-07-02 09:49:38 +0200

    links & scripts

--- index.php
+++ index.php
@@ -9,8 +9,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Php Ajax Dischi</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="./dist/css/app.css">
 </head>
 <body>
     
+
+    <!-- VUE JS -->
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.js"></script>
+    <!-- AXIOS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js" 
+    integrity="sha512-bZS47S7sPOxkjU/4Bt0zrhEtWx0y0CRkhEp8IckzK+ltifIIE9EMIMTuT/mEzoIMewUINruDBIR/jJnbguonqQ==" 
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- JS -->
+    <script src="./dist/js/app.js"></script>
 </body>
 </html>
